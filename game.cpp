9508f31b9ff2c5069d105cece7643d06d661ee37
Nathan <nathanluc00@gmail.com>
2019-09-27 15:11:37 -0700

    some changes

--- game.cpp
+++ game.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 Game::Game()
-{ 
+{
     humanPlayer = new Player("Player1", Option::ROCK, 0);
     compPlayer = new Computer();
-    activeGame = false; 
+    activeGame = false;
 }
 
 void Game::runGame()
@@ -15,6 +15,7 @@ void Game::runGame()
     gameStart();
 
     string nameInput;
+    int round_count = 0;
     cout << "Enter your name: ";
     cin >> nameInput;
     humanPlayer->setUsername(nameInput);
@@ -35,16 +36,16 @@ void Game::runGame()
     {
         cout << "----------------------------------" << endl;
         displayOptions();
-    
+
         compPlayer->generateOption();
-    
+
         cout << "Computer chose " << compPlayer->getOption() << endl;
-    
+
         int winner = evaluateUserWin();
         switch(winner)
         {
-            case 0: 
-                cout << "It was a TIE!" << endl; 
+            case 0:
+                cout << "It was a TIE!" << endl;
                 break;
             case 1:
                 cout << "YOU Won!" << endl;
@@ -55,9 +56,14 @@ void Game::runGame()
                 compPlayer->incrementScore();
                 break;
         }
-    
+
         displayScore();
-    }while((compPlayer->getScore() < 10) && (humanPlayer->getScore() < 10));
+        round_count++;
+        if(round_count == 20)
+        {
+          break;
+        }
+    }while((compPlayer->getScore() < 20) && (humanPlayer->getScore() < 20));
 
     // Step 7: End Game
     gameEnd();
@@ -67,7 +73,7 @@ void Game::displayScore()
 {
     if(activeGame)
     {
-        cout << endl 
+        cout << endl
             << "SCORE"                                  << endl
             << "----------------------"                 << endl
             << humanPlayer->getUsername() << ": " << humanPlayer->getScore()  << endl
@@ -101,26 +107,26 @@ void Game::displayOptions()
             validInput = true;
 
             cout << "Choose an option: " << endl
-                << "1. Rock"        << endl 
+                << "1. Rock"        << endl
                 << "2. Paper"       << endl
                 << "3. Scissors"    << endl
                 << "Choice (1-3): ";
             cin >> userInput;
             userInput--;
             cout << endl << endl;
-    
+
             switch(static_cast<Option>(userInput))
             {
                 case Option::ROCK:      cout << "You chose " << Option::ROCK     << endl; break;
                 case Option::PAPER:     cout << "You chose " << Option::PAPER    << endl; break;
                 case Option::SCISSORS:  cout << "You chose " << Option::SCISSORS << endl; break;
-                default: 
+                default:
                     cout << "Invalid Input! Try Again!" << endl;
-                    validInput = false; 
+                    validInput = false;
                     break;
             }
         } while(!validInput);
-    
+
         humanPlayer->setOption(static_cast<Option>(userInput));
     }
 }
@@ -133,22 +139,23 @@ int Game::evaluateUserWin()
 
     if(activeGame)
     {
+        if(humanPlayer->getOption() == compOption)
+        {
+          winner  = 0;
+        }
         switch(humanPlayer->getOption())
         {
-            case (Option::ROCK): 
-                if     (compOption == Option::ROCK)     winner = 0; 
-                else if(compOption == Option::PAPER)    winner = 2;
-                else if(compOption == Option::SCISSORS) winner = 1;
+            case (Option::ROCK):
+                if(compOption == Option::PAPER)         winner = 2;
+                else                                    winner = 1;
                 break;
-            case (Option::PAPER): 
-                if     (compOption == Option::ROCK)     winner = 1;            
-                else if(compOption == Option::PAPER)    winner = 0;
-                else if(compOption == Option::SCISSORS) winner = 2;
+            case (Option::PAPER):
+                if     (compOption == Option::ROCK)     winner = 1;
+                else                                    winner = 2;
                 break;
-            case (Option::SCISSORS): 
+            case (Option::SCISSORS):
                 if     (compOption == Option::ROCK)     winner = 2;
-                else if(compOption == Option::PAPER)    winner = 1;
-                else if(compOption == Option::SCISSORS) winner = 0;
+                else                                    winner = 1;
                 break;
         }
     }
